7d66cbf84e152fe1c4682ef3a051344cffcab124
Veronique Cadet <cadetvero@gmail.com>
2023-01-24 18:26:10 -0500

    sinatra connected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  
+  get "/users" do
+  User.all.to_json
   end
 
 end
